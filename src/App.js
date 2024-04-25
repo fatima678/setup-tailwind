764a8cc8a445a74ff4a0d1fbc68a63c9b0ab441c
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-04-25 19:51:13 +0500

    Implement onclick function to display the alert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import logo from "./img/logo.svg"
 
 function App() {
+  let displayData=()=>{
+    alert("Welcome to Ws")
+  }// simple function
+  let addData=(a,b)=>{
+    console.log(a+b)
+  }// parameterized function
+  
   return (
     <div className="App">
-      <h1 className='text-[50px] text-red-700 font-bold'>Welcome to Ws</h1>
+      
+      <img width={300} src= {logo} alt='' />
+      <h1 className='text-[50px] text-blue-700 font-bold' >Welcome to WSCube</h1>
+      <button className='bg-[red] p-[30px]' onClick={displayData}>Save</button>
+      <button className='bg-[red] p-[30px] ' onClick={()=>addData(20,25)}>Add data</button>
     </div>
   );
 }
